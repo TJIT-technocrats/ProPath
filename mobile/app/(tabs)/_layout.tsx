e371d5f671672bdb441b8583237f73b48c7725d9
Nikhil <nikhil.4002.50.82@gmail.com>
2025-08-30 19:54:15 +0530

    added icons

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -1,22 +1,25 @@
 import React from "react";
 import { Tabs } from "expo-router";
-import AntDesign from "react-native-vector-icons/AntDesign";
-import FontAwesome from "react-native-vector-icons/FontAwesome";
 import Ionicons from "react-native-vector-icons/Ionicons";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 
 const NavLayout: React.FC = () => {
   return (
     <Tabs
       screenOptions={{
-        tabBarShowLabel: false,
+        // tabBarShowLabel: false,
+        tabBarStyle: {
+          paddingBottom: 5,
+          paddingTop: 5,
+        },
       }}
     >
       <Tabs.Screen
         name="index"
         options={{
           headerShown: false,
-          tabBarIcon: ({ focused, color, size }) => (
-            <AntDesign name="home" size={size} color={color} />
+          tabBarIcon: ({ focused, color, size = 40 }) => (
+            <Ionicons name="home" color={color} size={size} />
           ),
         }}
       />
@@ -24,8 +27,8 @@ const NavLayout: React.FC = () => {
         name="PlacementInfo"
         options={{
           headerShown: false,
-          tabBarIcon: ({ focused, color, size }) => (
-            <Ionicons name="book" color={color} size={size} />
+          tabBarIcon: ({ focused, color, size = 40 }) => (
+            <MaterialIcons name="library-books" color={color} size={size} />
           ),
         }}
       />
@@ -33,8 +36,8 @@ const NavLayout: React.FC = () => {
         name="Profile"
         options={{
           headerShown: false,
-          tabBarIcon: ({ focused, color, size = "32" }) => (
-            <FontAwesome name="user" color={color} size={size} />
+          tabBarIcon: ({ focused, color, size = 40 }) => (
+            <Ionicons name="person-circle-sharp" color={color} size={30} />
           ),
         }}
       />
