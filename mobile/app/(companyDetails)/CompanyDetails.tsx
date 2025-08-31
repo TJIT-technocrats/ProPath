2ced329d9a78cd124b9c7922be398ec84ff34476
Nikhil <nikhil.4002.50.82@gmail.com>
2025-08-31 18:40:02 +0530

    Update CompanyDetails.tsx

--- mobile/app/(companyDetails)/CompanyDetails.tsx
+++ mobile/app/(companyDetails)/CompanyDetails.tsx
@@ -1,12 +1,25 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView } from "react-native";
+import React from "react";
 
 const CompanyDetails = () => {
   return (
-    <View className='flex items-center justify-center h-[100vh]'>
-      <Text className='text-3xl'>CompanyDetails</Text>
-    </View>
-  )
-}
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <View className="w-full">
+        <View className="bg-blue-600 h-[12em] px-4 pl-14 pt-14">
+          <View className="mb-2">
+            <Text className="text-2xl font-bold text-white">Dhee Coding Labs</Text>
+            <Text className="text-gray-200 text-lg">Interns</Text>
+          </View>
+          <View className="flex flex-row items-center">
+            <Text className="text-xl font-semibold text-white mr-6">
+              4-12 LPA
+            </Text>
+            <Text className=" text-gray-200 text-md">September 2</Text>
+          </View>
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
 
-export default CompanyDetails
+export default CompanyDetails;
